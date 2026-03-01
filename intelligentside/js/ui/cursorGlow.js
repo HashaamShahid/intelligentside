1eda23df83c080ba63de232c36ac7bae247c5df2
HashaamShahid <hsyz2019@gmail.com>
2026-03-02 00:07:27 +0500

    Delete intelligentside directory

--- intelligentside/js/ui/cursorGlow.js
+++
@@ -1,8 +0,0 @@
-export function initCursorGlow(){
-  const glow = document.getElementById("glow");
-  if(!glow) return;
-  window.addEventListener("mousemove", (e) => {
-    glow.style.left = e.clientX + "px";
-    glow.style.top = e.clientY + "px";
-  }, { passive:true });
-}
